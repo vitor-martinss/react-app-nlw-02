4b142e53b3f950cb22c2a5838a996d63cbe4ef2e
Vitor Martins <vitor.martins@fever.pt>
2020-08-07 12:11:24 +0100

    UPD finished integration TeacherForm page

--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../TeacherItem';
-
+import Input from '../../components/Input'
+import Select from '../../components/Select';
 import './styles.css'
 
 
@@ -9,21 +10,41 @@ function TeacherList() {
 	return (
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Estes são os proffys disponíveis">
-				<form action="" id="search-teachers">
-					<div className="input-block">
-						<label htmlFor="subject">Matéria</label>
-						<input type="text" id="subject"/>
-					</div>
-
-					<div className="input-block">
-						<label htmlFor="week_day">Dia da semana</label>
-						<input type="text" id="week_day"/>
-					</div>
-
-					<div className="input-block">
-						<label htmlFor="time">Matéria</label>
-						<input type="text" id="time"/>
-					</div>
+				<form id="search-teachers">
+					<Select
+						name="subject"
+						label="Matéria"
+						//value={subject}
+						//onChange={e => setSubject(e.target.value)}
+						options={[
+						{ value: 'Artes', label: 'Artes' },
+						{ value: 'Biologia', label: 'Biologia' },
+						{ value: 'Ciências', label: 'Ciências' },
+						{ value: 'Educação Física', label: 'Educação Física' },
+						{ value: 'Física', label: 'Física' },
+						{ value: 'Geografia', label: 'Geografia' },
+						{ value: 'História', label: 'História' },
+						{ value: 'Matemática', label: 'Matemática' },
+						{ value: 'Português', label: 'Português' },
+						{ value: 'Química', label: 'Química' },
+						]}
+					/>
+					<Select
+						name="week_day"
+						label="Dia da semana"
+						//value={week_day}
+						//onChange={e => setWeekDay(e.target.value)}
+						options={[
+						{ value: '0', label: 'Domingo' },
+						{ value: '1', label: 'Segunda-feira' },
+						{ value: '2', label: 'Terça-feira' },
+						{ value: '3', label: 'Quarta-feira' },
+						{ value: '4', label: 'Quinta-feira' },
+						{ value: '5', label: 'Sexta-feira' },
+						{ value: '6', label: 'Sábado' },
+						]}
+					/>
+					<Input name='time' label='Hora' />
 				</form>
 			</PageHeader>
 

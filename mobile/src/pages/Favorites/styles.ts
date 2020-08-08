7dd9cb9325eadf9dbe41c388514a66ff2d361d10
Vitor Martins <vitor.martins@fever.pt>
2020-08-08 16:00:56 +0100

    UPD styles navigation tabs

--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -3,9 +3,11 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		backgroundColor: '#8257e5',
-		padding: 40
+		backgroundColor: '#f0f0f7',
+	},
+
+	teacherList: {
+		marginTop: -40,
 	},
 
 })
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -3,9 +3,7 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		backgroundColor: '#8257e5',
-		padding: 40
+		backgroundColor: '#f0f0f7',
 	},
 
 })
